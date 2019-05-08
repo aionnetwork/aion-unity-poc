ea3a05cd3c5e8457e9371e8719b62e14ab3775e0
gzhong <ge@aion.network>
2019-05-08 14:17:06 -0400

    initial staking contract

---
+++ solidity/staking_registry.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.4.15;
+
+contract StakingRegistry {
+    
+    address private owner;
+    
+    uint private baseTarget;
+    bytes32 private seed;
+    
+    address[] public stakers;
+    mapping(address => uint) private votes;
+    mapping(address => mapping (address => uint)) private stakes;
+    
+    function StakingRegistry() {
+        owner = msg.sender;
+        baseTarget = 1;
+    }
+    
+    function register(address stakingAddress) {
+        stakers.push(stakingAddress);
+    }
+    
+    function vote(address stakingAddress) public payable {
+        votes[stakingAddress] += msg.value;
+        stakes[msg.sender][stakingAddress] += msg.value;
+    }
+    
+    function unvote(address stakingAddress, uint amount) public {
+        uint currAmount = stakes[msg.sender][stakingAddress];
+        uint currVote = votes[stakingAddress];
+        if (amount < currAmount) {
+            currAmount -= amount;
+            currVote -= amount;
+            votes[stakingAddress] = currVote;
+            stakes[msg.sender][stakingAddress] = currAmount;
+            msg.sender.transfer(amount);
+        } else {
+            votes[stakingAddress] = currVote - currAmount;
+            stakes[msg.sender][stakingAddress] = 0;
+            msg.sender.transfer(currAmount);
+        }
+    }
+    
+    function getVote(address stakingAddress) returns (uint) {
+        return votes[stakingAddress];
+    }
+    
+    function getBaseTarget() public returns (uint) {
+        return baseTarget;
+    }
+    
+    function setBaseTarget(uint newTarget) {
+        baseTarget = newTarget;
+    }
+    
+    function getSeed() public returns (bytes32) {
+        return seed;
+    }
+    
+    function setSeed(bytes32 newSeed) {
+        seed = newSeed;
+    }
+}
