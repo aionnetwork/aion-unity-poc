c685deb68a2fe3d38de9468c6198eb7d25f3fabc
gzhong <ge@aion.network>
2019-05-15 17:13:08 -0400

    update deploy.js

--- solidity/deploy.js
+++ solidity/deploy.js
@@ -9,9 +9,11 @@ node call vote [address] [value]
 */
 
 console.log("args:" + args);
-let nodeUrl = 'http://127.0.0.1:8545';
+let nodeUrl = 'http://127.0.0.1:9001';
 let accountAddress = '0xa00a2D0D10ce8a2EA47A76fBb935405df2a12b0e2BC932F188F84b5f16da9C2C';
-let contractAddress = '0xA05c87D3cfA28a3b2aDae9E4e6e4A850A27B15A252fEcb076650ee28D726dc31'
+//let contractAddress = '0xA05c87D3cfA28a3b2aDae9E4e6e4A850A27B15A252fEcb076650ee28D726dc31'
+let contractAddress = '0xA00876bE75B664DE079B58E7acBf70CE315Ba4aAa487F7DdF2Abd5e0e1A8dFf4'
+
 let defaultPassword = 'password';
 web3 = new Web3(new Web3.providers.HttpProvider(nodeUrl));
 personal = web3.eth.personal;
@@ -22,8 +24,8 @@ let stakingSol = fs.readFileSync("staking_registry.sol", "utf8");
 web3.eth.compileSolidity(stakingSol).then((res) => unlockAccount(res));
 
 function unlockAccount(compiled) {
-    web3.eth.personal.unlockAccount( accountAddress, defaultPassword, 500)
-    .then(processCommand(compiled))
+    web3.eth.personal.unlockAccount( accountAddress, defaultPassword, 5000)
+    .then(processCommand(compiled)).catch(console.log)
 }
 
 function processCommand(compiled) {
@@ -53,22 +55,48 @@ function callMethod(contractInst) {
             registerAccount = accountAddress;
         }
         console.log("register account:" +  registerAccount);
-        let method = contractInst.methods.register(accountAddress);
+        let method = contractInst.methods.register(registerAccount);
         method.estimateGas(
             {from: accountAddress}
-            ).then((gasAmount) => send(method, gasAmount*2), null);
+            ).then((gasAmount) => send(method, gasAmount*2, null));
     } else if (args[1] == 'vote') {
         console.log("voting for: " + args[2] + " value: " + args[3] );
-        let method = contractInst.methods.vote(accountAddress);
+        let method = contractInst.methods.vote(args[2]);
+        method.estimateGas({from: accountAddress}
+        ).then((gasAmount) => send(method, gasAmount*2, args[3])).catch(console.log);
+    } else if (args[1] == 'getvote') {
+        console.log("get vote for: " + args[2]);
+        let method = contractInst.methods.getVote(args[2]);
         method.estimateGas(
             {from: accountAddress}
-        ).then((gasAmount) => send(method, gasAmount*2), args[3]);
+        ).then((gasAmount) => call(method, gasAmount*2));
+    } else {
+        web3.eth.getBlockNumber().then((num) => {
+            console.log("current block number: " + num)
+            getBlock(accountAddress, num-30, num-20)
+        })
     }
 }
 
-function send(method, gasAmount, value) {
+function call(method, gasAmount) {
     console.log("gas amount " + gasAmount);
+    method.call(
+        {from: accountAddress, gas: gasAmount}
+    ).then(console.log).catch(console.log)
+}
+
+function send(method, gasAmount, value) {
+    console.log("gas amount " + gasAmount + " value: " + value);
     method.send(
         {from: accountAddress, gas: gasAmount, value: value}
     ).then(console.log).catch(console.log)
 }
+
+function getBlock(address, start, end) {
+    for (var i = start; i <= end; i++) {
+        web3.eth.getBlock(i).then( (block) => {
+            console.log(block);
+        });
+    }
+}
+
